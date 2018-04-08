875dc0b3354f99697f84e08ad9a963f04dc1cf05
Scnsh <redheru+dev@gmail.com>
2018-04-08 23:36:46 +0900

    動作を確認、アイコンを変更、テキストエリアの領域を広げる

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MChromeAddon
 My Chrome Addon 
+
+## 利用素材
+- アイコン
+  - http://icooon-mono.com/12555-%E3%82%AB%E3%83%AC%E3%83%B3%E3%83%80%E3%83%BC%E3%82%A2%E3%82%A4%E3%82%B3%E3%83%B38/
--- js/popup.js
+++ js/popup.js
@@ -9,13 +9,19 @@ $(function(){
     var todayCurMonth = curDate.getMonth();	// 月を取得(0月～11月)
     var curYear = curDate.getFullYear();	// 年を取得        
 
+    var ids = '';    //読み込んだオフィス名に対応するidを取得
     var names = '';  //読み込んだオフィス名の文字列を取得
     var colors = ''; //読み込んだ色の文字列を取得
+
+    var myWeek;
+    var myTblLine;
+    var pukiwikiTable;
 
     //リストは2個以上が必要、0:休日用デフォルト、1:平日用デフォルト
     $.getJSON("js/list.json", function(data){
         for(var i in data)
         {
+            ids += data[i].id + ',';
             names += data[i].name + ',';
             colors += data[i].color + ',';
         }
@@ -25,22 +31,42 @@ $(function(){
     });
     
     //pukiwikiフォーマットの日付を出力する
-    function getDayPrint(id, day)
+    function getDayPrint(colorList, day)
     {
-        return "&color(" + colorList[i] + ",white){" + day +"};";
+        if(day == '')
+            return '';
+
+        var id = $("[id=list" + day +  "]").val();
+        return "&color(" + colorList[id] + ",white){" + day +"};";
     }
 
     //pukiwiki用のフォーマットを出力する
-    function getPukiwiki()
+    function getPukiwiki(tblLine, table)
     {
         var colorList = colors.split(",");
         var nameList = names.split(",");
         var txt = '';
         for(var i=0; i < nameList.length - 1; i++)
         {
-            txt += "- &color(" + colorList[i] + ",white){" + colorList[i] + " : " + nameList[i] + "};¥n"   
+            txt += "- &color(" + colorList[i] + ",white){" + colorList[i] + " : " + nameList[i] + "};\n"   
         }
-        txt += "|月|火|水|木|金|土|日|¥n";
+        txt += "|月|火|水|木|金|土|日|\n";
+
+        var source = '';
+        var td = '|';
+        var tdC = '|';
+        var tr = '|';
+        var trC = '\n';
+    
+        for(i=0; i<tblLine; i++){	// 表の「行」のループ
+            source += tr;
+            for (var j = 0; j < 7; j++) {
+                var dat = table[j+(i*7)];
+                source += getDayPrint(colorList, dat) + tdC;
+            }
+            source += trC;
+        }
+        txt += source;
         return txt;
     }
 
@@ -49,6 +75,7 @@ $(function(){
     {
         var txt = '<select id="list' + (day + 1) + '">';
         var nameList = names.split(",");
+        var idList = ids.split(",");
         var index = day+myWeek;
 
         var selector = new Array();
@@ -62,17 +89,17 @@ $(function(){
         }
         
         txt += '<option value="' + 
-               nameList[0] + '"' + 
+               idList[0] + '"' + 
                selector[0] + '>' + 
                nameList[0] + '</option>';
         txt += '<option value="' + 
-               nameList[1] + '"' + 
+               idList[1] + '"' + 
                selector[1] + '>' + 
                nameList[1] + '</option>';
         for(var i = 2; i < nameList.length; i++)
         {
             txt += '<option value="' + 
-            nameList[i] + '">' + 
+            idList[i] + '">' + 
             nameList[i] + '</option>';         
         }
         txt += '</select>';    
@@ -87,14 +114,17 @@ $(function(){
         ((myYear % 4)===0 && (myYear % 100) !==0) || (myYear % 400)===0 ? monthTbl[1] = 29: 28;	// うるう年だったら2月を29日とする
         var myMonth = myDate.getMonth();	// 月を取得(0月～11月)
         myDate.setDate(1);	// 日付を'1日'に変えて、
-        var myWeek = myDate.getDay() == 0 ? 6 : myData.getDay() -1;	// '１日'の曜日を取得(0:月 -> 6:日)
-        var myTblLine = (myWeek < 0 && monthTbl[myMonth] > 29) ? 6 : Math.ceil((myWeek+monthTbl[myMonth])/7);	// カレンダーの行数, 日曜から始まり30日以上ある月のみ6行
+        myWeek = myDate.getDay() == 0 ? 6 : myData.getDay() -1;	// '１日'の曜日を取得(0:月 -> 6:日)
+        myTblLine = (myWeek < 0 && monthTbl[myMonth] > 29) ? 6 : Math.ceil((myWeek+monthTbl[myMonth])/7);	// カレンダーの行数, 日曜から始まり30日以上ある月のみ6行
         var myTable = new Array(7*myTblLine);	// 表のセル数を用意
+        pukiwikiTable = new Array(7*myTblLine);	// 表のセル数を用意        
         for(var i=0; i<7*myTblLine; i++){
             myTable[i]=' ';	// セルを全て空にする
+            pukiwikiTable[i]='';
         }
         for(i=0; i<monthTbl[myMonth]; i++){
             myTable[i+myWeek]=(i+1) + getPulldown(myTable, i, myWeek);	// 日付を埋め込む
+            pukiwikiTable[i+myWeek]=(i+1);
         }
     
         var source = '';
@@ -148,7 +178,8 @@ $(function(){
 
         // $("#text").text("曜日:" + weekdays[firstday]);
         //$("#text").text($("#list15 option:selected").text());
-        $("#text").text(getPukiwiki());
+        $("#text").text(getPukiwiki(myTblLine, pukiwikiTable));
+
     });    
 
   }); 
